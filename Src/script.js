a83b3223972cebddba933c24a5122790010a5ce2
MisaelChristoph007 <122249229+MisaelChristoph007@users.noreply.github.com>
2023-11-07 13:48:50 +0000

    Pushingg

--- Src/script.js
+++ Src/script.js
@@ -137,3 +137,147 @@ document.getElementById('cart').addEventListener('click', () => {
   updateCart();
   $('#cartPopup').modal('show');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const listBarang = document.getElementById("listBarang");
+  const formItem = document.getElementById("formItem");
+  const keywordInput = document.getElementById("keyword");
+  let totalBarangDiKeranjang = 0;
+
+  var items = [
+['001', 'Keyboard Logitech', 60000, 'Keyboard yang mantap untuk kantoran', '/assets/logitek.jpg'],
+['002', 'Keyboard MSI', 300000, 'Keyboard gaming MSI mechanical', '/assets/msi.jpg'],
+['003', 'Mouse Genius', 50000, 'Mouse Genius biar lebih pintar', '/assets/genius.jpeg'],
+['004', 'Mouse Jerry', 30000, 'Mouse yang disukai kucing', '/assets/jerry.jpg']
+]
+
+  function Cari(keyword) {
+      listBarang.innerHTML = "";
+      for (let i = 0; i < items.length; i++) {
+          const item = items[i];
+          if (item[1].toLowerCase().includes(keyword.toLowerCase())) {
+              const card = document.createElement("div");
+              card.classList.add("col-4", "mt-2");
+              card.innerHTML = `
+                  <div class ="col-4 mt-2"> 
+                      <div class="card" style="width: 18rem;">
+                          <img src="${item[4]}" class="card-img-top" height="200px" width="200px" alt="...">
+                          <div class="card-body">
+                              <h5 class="card-title" id="itemName">${item[1]}</h5>
+                              <p class="card-text" id="itemDesc">${item[3]}</p>
+                              <p class="card-text">Rp.${item[2]}</p>
+                              <br>
+                              <a href="#" class="btn btn-primary add-to-cart" data-id="${item[0]}">Tambahkan ke keranjang</a>
+                          </div>
+                      </div>
+                  </div>
+              `;
+              listBarang.appendChild(card);
+          }
+      }
+      const btnTambahKeKeranjang = document.querySelectorAll(".add-to-cart");
+      btnTambahKeKeranjang.forEach(button => {
+          button.addEventListener("click", tambahKeKeranjang);
+      });
+  }
+  // menangani ketika tombol submit diklik
+  formItem.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const keyword = keywordInput.value;
+      Cari(keyword);
+
+  });
+
+//cart
+document.getElementById("cart").addEventListener("click", function () {
+    if (totalBarangDiKeranjang > 0) {
+        const cartItems = cart.items;
+        if (cartItems.length > 0) {
+            let message = '<table class="table"><thead><tr><th>Nama Barang</th><th>Deskripsi</th><th>Harga</th></tr></thead><tbody>';
+            for (let i = 0; i < cartItems.length; i++) {
+                const item = cartItems[i];
+                message += `<tr><td>${item.name}</td><td>${item.description}</td><td>Rp.${item.price}</td></tr>`;
+            }
+            message += '</tbody></table>';
+            Swal.fire({
+                title: 'Keranjang Anda',
+                html: message,
+                icon: 'info',
+            });
+        } else {
+            Swal.fire(
+                'Info',
+                'Keranjang masih kosong',
+                'info'
+            );
+        }
+    } else {
+        Swal.fire(
+            'Info',
+            'Keranjang belum berisi barang',
+            'info'
+        );
+    }
+});
+
+
+  function tambahKeKeranjang(event) {
+      const itemId = event.target.getAttribute("data-id");
+      // untuk mencari item dari array items yang punya id yg sama dengan itemId
+      const item = items.find(item => item[0] === itemId);
+
+      if (totalBarangDiKeranjang < 9) {
+          totalBarangDiKeranjang++;
+      } else {
+          totalBarangDiKeranjang = `9+`;
+      }
+
+      document.getElementById("cartItemCount").textContent = `(${totalBarangDiKeranjang})`;
+      Swal.fire(
+          'Success',
+          `${item[1]} Berhasil ditambahkan keranjang!`,
+          'success'
+      )
+  }
+  // Menampilkan semua barang saat halaman dimuat
+  Cari("");
+});
+
+// Di dalam event handler untuk tombol cari, tambahkan kode berikut
+formItem.addEventListener("submit", function (e) {
+e.preventDefault();
+const keyword = keywordInput.value;
+Cari(keyword);
+
+// Periksa apakah hasil pencarian kosong
+if (listBarang.childElementCount === 0) {
+    tampilkanPesanProdukTidakTersedia(keyword);
+} else {
+    // Hapus pesan produk tidak tersedia jika ada hasil pencarian
+    const pesanProdukTidakTersedia = document.querySelector(".no-result-message");
+    if (pesanProdukTidakTersedia) {
+        pesanProdukTidakTersedia.remove();
+    }
+}
+});
+
+// Function to handle "Clear All" button click
+document.getElementById('clearCartButton').addEventListener('click', function () {
+Swal.fire({
+    title: 'Konfirmasi',
+    text: 'Anda yakin ingin mengosongkan keranjang?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Ya',
+    cancelButtonText: 'Batal'
+}).then((result) => {
+    if (result.isConfirmed) {
+        // Mengosongkan keranjang
+        totalBarangDiKeranjang = 0;
+        document.getElementById("cartItemCount").textContent = `(0)`;
+        Swal.fire('Keranjang dikosongkan', 'Semua barang telah dihapus dari keranjang!', 'success');
+    }
+});
+});
